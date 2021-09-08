5d95c8d21575e3ad13ec213a80632f86abfe3451
André Luis dos Santos <alsss.ads@gmail.com>
2021-09-08 17:40:36 -0300

    :heavy_plus_sign:  chore: Adiciona font awesome icon pack

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {  } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.use(Buefy)
+
+library.add()
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
