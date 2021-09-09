2674e628092376dd4757b15eab0414b6fb08a12d
André Luis dos Santos <alsss.ads@gmail.com>
2021-09-09 18:13:03 -0300

    :heavy_plus_sign: chore: Adiciona Axios

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
 
+import Axios from 'axios'
+
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserCircle, faBell, faAngleDown, faAngleUp, faFilter, faChartLine, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.prototype.$http = Axios
+Axios.defaults.baseURL = '/'
 
 Vue.use(Buefy,{
   defaultIconPack: 'fa',
