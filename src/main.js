78e515c59921f133edd4c206a67e1bd3dbd2d6b0
André Luis dos Santos <alsss.ads@gmail.com>
2021-09-10 21:31:04 -0300

    :wrench: chore: Configura cores do Buefy/bulma

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import Axios from 'axios'
 import './server'
 
 import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
+import './assets/main.scss'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { 
